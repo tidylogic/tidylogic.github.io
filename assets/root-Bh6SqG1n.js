09e28d72e67a8124b83cf59fdce8ce6dd6f1348e
tidylogic <wndduq0000@gmail.com>
2025-12-17 01:16:40 +0900

    Updates

---
+++ assets/root-Bh6SqG1n.js
@@ -0,0 +1,21 @@
+import{w as r,q as i,p as t,M as c,L as l,S as p,t as h,O as u,i as d}from"./chunk-WWGJGFF6-BOKkXrnx.js";const x=()=>[{rel:"preconnect",href:"https://fonts.googleapis.com"},{rel:"preconnect",href:"https://fonts.gstatic.com",crossOrigin:"anonymous"},{rel:"stylesheet",href:"https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"}];function f({children:e}){return t.jsxs("html",{lang:"ko",children:[t.jsxs("head",{children:[t.jsx("meta",{charSet:"utf-8"}),t.jsx("meta",{name:"viewport",content:"width=device-width, initial-scale=1"}),t.jsx(c,{}),t.jsx(l,{}),t.jsx("script",{type:"text/javascript",dangerouslySetInnerHTML:{__html:`
+              // Single Page Apps for GitHub Pages
+              // MIT License
+              // https://github.com/rafgraph/spa-github-pages
+              (function(l) {
+                if (l.search[1] === '/') {
+                  var decoded = l.search
+                    .slice(1)
+                    .split('&')
+                    .map(function(s) {
+                      return s.replace(/~and~/g, '&')
+                    })
+                    .join('?')
+                  window.history.replaceState(
+                    null,
+                    null,
+                    l.pathname.slice(0, -1) + decoded + l.hash
+                  )
+                }
+              })(window.location)
+            `}})]}),t.jsxs("body",{children:[e,t.jsx(p,{}),t.jsx(h,{})]})]})}const g=r(function(){return t.jsx(u,{})}),j=i(function({error:s}){let o="Oops!",n="An unexpected error occurred.",a;return d(s)&&(o=s.status===404?"404":"Error",n=s.status===404?"The requested page could not be found.":s.statusText||n),t.jsxs("main",{className:"pt-16 p-4 container mx-auto",children:[t.jsx("h1",{children:o}),t.jsx("p",{children:n}),a]})});export{j as ErrorBoundary,f as Layout,g as default,x as links};
